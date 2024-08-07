a75ee2e370c2818f58e778123aada2e2f59094c3
Hanif Carroll <hanifcarroll@gmail.com>
2024-08-06 21:42:28 -0300

    Add sidebar

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,67 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Box, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PeopleIcon from '@mui/icons-material/People';
+import PaymentIcon from '@mui/icons-material/Payment';
+import HistoryIcon from '@mui/icons-material/History';
+import CardMembershipIcon from '@mui/icons-material/CardMembership';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
 
-const Sidebar = () => {
-  return <div>Sidebar</div>
+const drawerWidth = 240;
+
+const Sidenav = () => {
+  const pathname = usePathname();
+
+  const menuItems = [
+    { text: 'Dashboard', icon: <DashboardIcon/>, path: '/' },
+    { text: 'Register Member', icon: <PersonAddIcon/>, path: '/register' },
+    { text: 'Member List', icon: <PeopleIcon/>, path: '/members' },
+    { text: 'Process Payment', icon: <PaymentIcon/>, path: '/payment' },
+    { text: 'Payment History', icon: <HistoryIcon/>, path: '/payment-history' },
+    { text: 'Membership Plans', icon: <CardMembershipIcon/>, path: '/plans' },
+    { text: 'Member Check-in', icon: <HowToRegIcon/>, path: '/check-in' },
+  ];
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <Box sx={{ overflow: 'auto' }}>
+        <Box sx={{ p: 2 }}>
+          <Typography variant="h6" component="div">
+            Gym Management
+          </Typography>
+        </Box>
+        <Divider/>
+        <List>
+          {menuItems.map((item) => (
+            <ListItemButton
+              key={item.text}
+              component={Link}
+              href={item.path}
+              selected={pathname === item.path}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text}/>
+            </ListItemButton>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
 };
 
-export default Sidebar;
+export default Sidenav;
